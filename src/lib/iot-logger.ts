54c3975a2e55c7446cda9f2f09422b65aba2020b
Rodrigo de Souza <rsouza01@gmail.com>
2019-12-10 19:36:25 +0100

    Formatter added

--- src/lib/iot-logger.ts
+++ src/lib/iot-logger.ts
@@ -2,31 +2,53 @@
 
 import uuid from 'uuid/v4';
 import moment from 'moment';
+import winston from 'winston';
+
+const { createLogger, format, transports } = winston;
+
+const iotLoggerFormat = format.printf(({ level, message, label, timestamp }) => {
+  return `[${timestamp}] [${label}] ${level}: ${message}`;
+});
+
 
 export default class IoTLogger {
-  constructor() {
+
+  logger: winston.Logger;
+
+  constructor(filename: string) {
     const loggingApplicationName = process.env.ApplicationName;
     const loggingApplicationVersion = process.env.ApplicationVersion;
     const transactionId = uuid();
+
+    const logFormat = format.combine(
+      format.colorize(),
+      format.label({ label: loggingApplicationName }),
+      format.timestamp(),
+      iotLoggerFormat
+    );
+
+    this.logger = winston.createLogger({
+      format: logFormat, 
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename })
+      ]
+    });
   }
 
-  debug(message, data = {}) {
-    console.log(message);
+  debug(message: string, data = {}) {
+    this.logger.info(message);
   }
 
-  warning(error) {
-    console.log(error);
+  warning(message: string) {
+    this.logger.warn(message);
   }
 
-  error(error) {
-    console.log(error);
+  info(message: string) {
+    this.logger.info(message);
   }
 
-  fatal(error) {
-    console.log(error);
-  }
-
-  issue(error) {
-    console.log(error);
+  error(error: string) {
+    this.logger.error(error);
   }
 }
--- test/unit/iot-logger.unit.ts
+++ test/unit/iot-logger.unit.ts
@@ -1,11 +1,35 @@
 import { expect } from 'chai';
 import 'mocha';
+
+const proxyquire = require('proxyquire').noPreserveCache()
+
 import IoTLogger from '../../src/lib/iot-logger';
 
 describe('IotLogger', () => {
+  
+  const filename = 'logger.log';
+  
+  before(() => {
+    process.env.ApplicationName = 'ApplicationName';
+    process.env.ApplicationVersion = 'ApplicationVersion';
+  });
 
   it('Create instance', () => {
-    const iotLogger = new IoTLogger();
+    const iotLogger = new IoTLogger(filename);
     expect(iotLogger).not.to.be.undefined;
   });
+
+  it('Log debug', () => {
+    const iotLogger = new IoTLogger(filename);
+    expect(iotLogger).not.to.be.undefined;
+
+    iotLogger.info('What rolls down stairs');
+    iotLogger.info('What rolls down stairs');
+    iotLogger.info('alone or in pairs,');
+    iotLogger.info('and over your neighbors dog?');
+    iotLogger.warning('Whats great for a snack,');
+    iotLogger.info('And fits on your back?');
+    iotLogger.error('Its log, log, log');    
+  });
+
 });
